ad3078eb21a4044b7e908bd242785cdc7c4caa18
Tore Dessena <toredessena@icloud.com>
2022-11-14 11:16:39 +0100

    Selettore evidenziato

--- Components/SliderProvaComponentView.swift
+++ Components/SliderProvaComponentView.swift
@@ -23,59 +23,139 @@ struct SliderProvaComponentView: View {
     // MARK: - BODY
     
     var body: some View {
-        
-        VStack {
-            
-               HStack {
-                   Text(qualityDescriptionText)
-                       .foregroundColor(Color(colorComponent))
-                   Spacer()
-                   switch sliderValue {
-                   case -0.20:
-                       Text("Scarsa")
-                           .bold()
-                   case -0.10:
-                       Text("Mediocre")
-                           .bold()
-                   case 0.00:
-                       Text("Ordinaria")
-                           .bold()
-                   case 0.10:
-                       Text("Discreta")
-                           .bold()
-                   case 0.20:
-                       Text("Buona")
-                           .bold()
-                   default:
-                       Text("Discreta")
-                           .bold()
-                   }
-               }
-            
-            Slider(value: sliderValueBinding, in: arc, step: istep)
-                .accentColor(Color(colorComponent))
-            
 
-               HStack {
-                   Text("Incidenza sul componente")
-                       .foregroundColor(Color(colorComponent))
-                   Spacer()
-                   Text("\(sliderValue * 100, specifier: "%.0f") %")
-                       .bold()
-                       .padding(4)
-                       .padding(.leading, 8)
-                       .padding(.trailing, 8)
-                       .background(Capsule(style: .circular).fill(isDarkMode ? Color(colorComponent) : Color.white))
-               }
+        VStack {
+            // prova scena
+            HStack {
+                Text(qualityDescriptionText)
+                    .foregroundColor(Color(colorComponent))
+                Spacer()
+                switch sliderValue {
+                case -0.20:
+                    scena
+                case -0.10:
+                    scena
+                case 0.00:
+                    scena
+                case 0.10:
+                    scena
+                case 0.20:
+                    scena
+                default:
+                    scena
+             
+                }
+            }
+            // fine prova scena
+            
+            /*
+            HStack {
+                Text(qualityDescriptionText)
+                    .foregroundColor(Color(colorComponent))
+                Spacer()
+                switch sliderValue {
+                case -0.20:
+                    Text("Scarsa")
+                        .bold()
+                case -0.10:
+                    Text("Mediocre")
+                        .bold()
+                case 0.00:
+                    Text("Ordinaria")
+                        .bold()
+                case 0.10:
+                    Text("Discreta")
+                        .bold()
+                case 0.20:
+                    Text("Buona")
+                        .bold()
+                default:
+                    Text("Discreta")
+                        .bold()
+                }
+            }
+             */
+         
+         Slider(value: sliderValueBinding, in: arc, step: istep)
+             .accentColor(Color(colorComponent))
+         
+
+            HStack {
+                Text("Incidenza sul componente")
+                    .foregroundColor(Color(colorComponent))
+                Spacer()
+                Text("\(sliderValue * 100, specifier: "%.0f") %")
+                    .bold()
+                    .padding(4)
+                    .padding(.leading, 8)
+                    .padding(.trailing, 8)
+                    .background(Capsule(style: .circular).fill(isDarkMode ? Color(colorComponent) : Color.white))
+                }
+            }
+        .padding()
         }
+    
+    var scena: some View {
+        HStack {
+            if sliderValue == -0.20 {
+                Text("S")
+            }
+                else {
+                    Text("S")
+                        .foregroundColor(.gray)
+                }
+            Spacer()
+            
+            if sliderValue == -0.10 {
+                Text("M")
+                    .foregroundColor(.pink)
+            }
+                else {
+                    Text("med")
+                        .foregroundColor(.gray)
+                }
+            Spacer()
+            
+            if sliderValue == 0.00 {
+                Text("O")
+                    .foregroundColor(.pink)
+            }
+                else {
+                    Text("ord")
+                        .foregroundColor(.gray)
+                }
+            Spacer()
+            if sliderValue == 0.10 {
+                Text("D")
+                    .foregroundColor(.pink)
+            }
+                else {
+                    Text("dis")
+                        .foregroundColor(.gray)
+                }
+            Spacer()
+            if sliderValue == 0.20 {
+                Text("B")
+                    .foregroundColor(.pink)
+            }
+                else {
+                    Text("buo")
+                        .foregroundColor(.gray)
+                }
+            }
+        }
+    
+    // ruler
+    
     }
-}
+    
+
 
 
  
  struct SliderProvaComponentView_Previews: PreviewProvider {
      static var previews: some View {
-         SliderConservationComponent(sliderValueBinding: .constant(0.10), arc: -0.20...0.20, istep: 0.10, sliderValue: 0.10, colorComponent: "ColorPinkBright", qualityDescriptionText: "Incidenza")
+         SliderProvaComponentView(sliderValueBinding: .constant(0.10), arc: -0.20...0.20, istep: 0.10, sliderValue: 0.10, colorComponent: "ColorPinkBright", qualityDescriptionText: "")
      }
  }
  
