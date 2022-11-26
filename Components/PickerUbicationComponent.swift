9a51bab3313de6e57d97a9e63a65840535411355
Tore Dessena <toredessena@icloud.com>
2022-11-26 18:50:16 +0100

    prova picker per ubication

---
+++ Components/PickerUbicationComponent.swift
@@ -0,0 +1,20 @@
+//
+//  PickerUbicationComponent.swift
+//  StimaCore
+//
+//  Created by salvatore dessena on 26/11/22.
+//
+
+import SwiftUI
+
+struct PickerUbicationComponent: View {
+    var body: some View {
+        Text("picker component")
+    }
+}
+
+struct PickerUbicationComponent_Previews: PreviewProvider {
+    static var previews: some View {
+        PickerUbicationComponent()
+    }
+}
