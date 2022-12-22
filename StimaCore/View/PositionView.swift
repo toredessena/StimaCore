c664568ebcf6731daf1f540355b0b40d397c4ad2
Tore Dessena <toredessena@icloud.com>
2022-12-22 15:06:43 +0100

    Picker Esposizione

--- StimaCore/View/PositionView.swift
+++ StimaCore/View/PositionView.swift
@@ -55,14 +55,14 @@ struct PositionView: View {
                 
                 // MARK: - PROVA PICKER
                 Section(header: Text("Ubicazione")) {
-                    Picker("seleziona", selection: $item.ubicazioneValue ) {
-                        Text("E").tag(-0.20)
-                        Text("C").tag(-0.10)
-                        Text("C-B").tag(0.00)
-                        Text("B").tag(0.10)
-                        Text("A-F").tag(0.20)
+                    Picker("", selection: $item.ubicazioneValue ) {
+                        Image(systemName: "e.circle").tag(-0.20)
+                        Image(systemName: "c.circle").tag(-0.10)
+                        Image(systemName: "b.circle").tag(0.00)
+                        Image(systemName: "a.circle").tag(0.10)
+                        Image(systemName: "f.circle").tag(0.20)
                     }.pickerStyle(.segmented)
-                    
+                       
                     VStack {
                         HStack {
                                 Text("Incidenza:")
@@ -72,21 +72,41 @@ struct PositionView: View {
                                                    .bold()
                                 }
                         Spacer(minLength: 4)
-                        HStack {
+                        HStack(alignment: .top) {
                             Text("Tipologia:")
                                 .foregroundColor(Color("ColorPinkBright"))
                             Spacer()
                             switch item.ubicazioneValue {
                             case -0.20:
-                            Text("Aree non urbanizzate")
+                            Text("Aree agricole, non urbanizzate, al di fuori dei centri abitati")
+                                    .font(.system(size: 12, weight: .bold))
+                                    .foregroundColor(.gray)
+                                    .multilineTextAlignment(.trailing)
+                                    .lineLimit(3)
                             case -0.10:
-                            Text("Espansioni incomplete")
+                                Text("Espansioni recenti, contigue al nucleo urbano consolidato")
+                                        .font(.system(size: 12, weight: .bold))
+                                        .foregroundColor(.gray)
+                                        .multilineTextAlignment(.trailing)
+                                        .lineLimit(3)
                             case 0.00:
-                            Text("Espansioni urbanizzate")
+                                Text("Espansioni e completamenti a ridosso dei centri storici")
+                                        .font(.system(size: 12, weight: .bold))
+                                        .foregroundColor(.gray)
+                                        .multilineTextAlignment(.trailing)
+                                        .lineLimit(3)
                             case 0.10:
-                            Text("Aree di completamento")
+                                Text("Aree di pregio in contesti residenziali storici")
+                                        .font(.system(size: 12, weight: .bold))
+                                        .foregroundColor(.gray)
+                                        .multilineTextAlignment(.trailing)
+                                        .lineLimit(3)
                             case 0.20:
-                            Text("Aree di pregio")
+                            Text("Contesti di lusso in aree turistiche di pregio")
+                                    .font(.system(size: 12, weight: .bold))
+                                    .foregroundColor(.gray)
+                                    .multilineTextAlignment(.trailing)
+                                    .lineLimit(3)
                             default:
                             Text("No")
                             }
@@ -98,7 +118,7 @@ struct PositionView: View {
                 }
                   
                     // MARK: -  ESPOSIZIONE
-                   
+                  /*
                 Section(header: Text("Esposizione")) {
                         SliderExpoComponentView(
                             sliderValueBinding: $item.esposizioneValue,
@@ -109,6 +129,70 @@ struct PositionView: View {
                             qualityDescriptionText: "Tipo:"
                         )
                     }
+                   */
+                // MARK: PROVA ESPOSIZIONE
+                Section(header: Text("Esposizione")) {
+                    Picker("", selection: $item.esposizioneValue ) {
+                        Image(systemName: "moonphase.new.moon").tag(-0.20)
+                        Image(systemName: "moonphase.waning.crescent").tag(-0.10)
+                        Image(systemName: "moonphase.last.quarter").tag(0.00)
+                        Image(systemName: "moonphase.waxing.crescent.inverse").tag(0.10)
+                        Image(systemName: "moonphase.full.moon").tag(0.20)
+                    }.pickerStyle(.segmented)
+                       
+                    VStack {
+                        HStack {
+                                Text("Incidenza:")
+                                .foregroundColor(Color("ColorPinkBright"))
+                                Spacer()
+                                Text("\(item.esposizioneValue * 100, specifier: "%.0f")%")
+                                                   .bold()
+                                }
+                        Spacer(minLength: 4)
+                        HStack(alignment: .top) {
+                            Text("Tipologia:")
+                                .foregroundColor(Color("ColorPinkBright"))
+                            Spacer()
+                            switch item.esposizioneValue {
+                            case -0.20:
+                            Text("Livello 0")
+                                    .font(.system(size: 12, weight: .bold))
+                                    .foregroundColor(.gray)
+                                    .multilineTextAlignment(.trailing)
+                                    .lineLimit(3)
+                            case -0.10:
+                                Text("Livello 1")
+                                        .font(.system(size: 12, weight: .bold))
+                                        .foregroundColor(.gray)
+                                        .multilineTextAlignment(.trailing)
+                                        .lineLimit(3)
+                            case 0.00:
+                                Text("Livello 2")
+                                        .font(.system(size: 12, weight: .bold))
+                                        .foregroundColor(.gray)
+                                        .multilineTextAlignment(.trailing)
+                                        .lineLimit(3)
+                            case 0.10:
+                                Text("Livello 3")
+                                        .font(.system(size: 12, weight: .bold))
+                                        .foregroundColor(.gray)
+                                        .multilineTextAlignment(.trailing)
+                                        .lineLimit(3)
+                            case 0.20:
+                            Text("Livello 4")
+                                    .font(.system(size: 12, weight: .bold))
+                                    .foregroundColor(.gray)
+                                    .multilineTextAlignment(.trailing)
+                                    .lineLimit(3)
+                            default:
+                            Text("No")
+                            }
+                        }
+                           
+                    }
+                    .padding(.vertical, 8)
+                   
+                }
                     
                 // MARK: - Accessibilità
                         Section(header: Text("Accessibilità")) {
